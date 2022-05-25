2832ba5bf66f9a73b657e5ef02d376e63c4df7f7
cuetodev <davidcueto.dev@gmail.com>
2022-05-25 08:21:55 +0200

    Setup

---
+++ TFG_Back/src/main/java/com/cuetodev/TFG_Back/TfgBackApplication.java
@@ -0,0 +1,13 @@
+package com.cuetodev.TFG_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgBackApplication.class, args);
+	}
+
+}
---
+++ TFG_Back/src/test/java/com/cuetodev/TFG_Back/TfgBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuetodev.TFG_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
